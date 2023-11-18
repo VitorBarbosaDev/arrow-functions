ef88751bc50b86f9821cf3cb2729f55674798d5a
VitorBarbosa1 <VITORBARBOSA1301@GMAIL.COM>
2023-11-18 22:18:20 +0000

    Completed Challenge

--- arrow-functions.js
+++ arrow-functions.js
@@ -42,3 +42,46 @@ const returnMultipleLines = () => (
 )
 
 console.log(returnMultipleLines());
+
+
+// Challenge 1
+
+let students = [
+	{
+		name: 'John',
+		subjects: ['maths', 'english', 'cad'],
+		teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+		results: {maths: 90, english: 75, cad: 87},
+	},
+	{
+		name: 'Emily',
+		subjects: ['science', 'maths', 'english', 'art'],
+		teacher: {science: 'Iris', maths: 'Harry', english: 'Joan', art: 'Simon'},
+		results: {science: 93, maths: 95, english: 80, art: 95},
+	},
+	{
+		name: 'Adam',
+		subjects: ['science', 'maths', 'art'],
+		teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+		results: {science: 63, maths: 79, art: 95},
+	}
+];
+
+const averagePoints = (arr,subject) => {
+	let total = 0;
+	let count = 0;
+	arr.forEach(student => {
+		if (student.results[subject]) {
+			total += student.results[subject];
+			count++;
+		}
+	});
+	return total / count;
+}
+
+// Declare a variable named averageMarks
+let averageMarks = averagePoints(students, 'english'); 
+// Log out the variable averageMarks to see its value
+console.log(averageMarks);
+
+
